c2c51c62c89d78ca9cabb00935987c06f5b66032
Drake Wiggins <drake_wiggins@outlook.com>
2024-11-10 13:51:54 -0800

    -Fix typos -Add comments -Add calibration opmodes -Fix licenses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMover.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMover.java
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2024 ChessMan14
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package org.firstinspires.ftc.teamcode;
 
 import static java.lang.Math.abs;
@@ -21,9 +45,9 @@ public class AutoMover {
     private final double back_right_motor_coefficient = 1;
 
     //You multiply the distance you want to move (or degrees you want to rotate) by these values to get a time in seconds you have to let the engines run for
-    private final double forward_distance_time_conv_fact = 1;
-    private final double backward_distance_time_conv_fact = 1;
-    private final double rotation_time_conv_fact = 1;
+    private final double forward_distance_time_conv_fact = 0.01;
+    private final double backward_distance_time_conv_fact = 0.01;
+    private final double rotation_time_conv_fact = 0.01;
 
     //Global speed percentage for all movement
     private final double speed_coefficient = 0.1;
@@ -41,7 +65,7 @@ public class AutoMover {
         this.back_right_motor.setDirection(DcMotor.Direction.FORWARD);
     }
 
-    public void move_forward(double distance) {
+    public double move_forward(double distance) {
         //How long the engines should run for
         double running_time = distance*forward_distance_time_conv_fact;
 
@@ -49,10 +73,10 @@ public class AutoMover {
         double start = runtime.seconds();
 
         //Send power to engines
-        front_left_motor.setPower(front_left_motor_coefficient* speed_coefficient);
-        front_right_motor.setPower(front_right_motor_coefficient* speed_coefficient);
-        back_left_motor.setPower(back_left_motor_coefficient* speed_coefficient);
-        back_right_motor.setPower(back_right_motor_coefficient* speed_coefficient);
+        front_left_motor.setPower(front_left_motor_coefficient*speed_coefficient);
+        front_right_motor.setPower(front_right_motor_coefficient*speed_coefficient);
+        back_left_motor.setPower(back_left_motor_coefficient*speed_coefficient);
+        back_right_motor.setPower(back_right_motor_coefficient*speed_coefficient);
 
         //Wait until done run time expired
         while (runtime.seconds() > (start + running_time)) {
@@ -64,9 +88,12 @@ public class AutoMover {
         front_right_motor.setPower(0);
         back_left_motor.setPower(0);
         back_right_motor.setPower(0);
+
+        //Used for telemetry and calibration purposes
+        return running_time;
     }
 
-    public void move_backward(double distance) {
+    public double move_backward(double distance) {
         //How long the engines should run for
         double running_time = distance*backward_distance_time_conv_fact;
 
@@ -75,10 +102,10 @@ public class AutoMover {
 
         //Send power to engines
         //Note the negative signs
-        front_left_motor.setPower(-front_left_motor_coefficient* speed_coefficient);
-        front_right_motor.setPower(-front_right_motor_coefficient* speed_coefficient);
-        back_left_motor.setPower(-back_left_motor_coefficient* speed_coefficient);
-        back_right_motor.setPower(-back_right_motor_coefficient* speed_coefficient);
+        front_left_motor.setPower(-front_left_motor_coefficient*speed_coefficient);
+        front_right_motor.setPower(-front_right_motor_coefficient*speed_coefficient);
+        back_left_motor.setPower(-back_left_motor_coefficient*speed_coefficient);
+        back_right_motor.setPower(-back_right_motor_coefficient*speed_coefficient);
 
         //Wait until done run time expired
         while (runtime.seconds() > (start + running_time)) {
@@ -90,10 +117,13 @@ public class AutoMover {
         front_right_motor.setPower(0);
         back_left_motor.setPower(0);
         back_right_motor.setPower(0);
+
+        //Used for telemetry and calibration purposes
+        return running_time;
     }
 
     //0 degrees is directly forward. To the left is negative
-    public void rotate(double degrees) {
+    public double rotate(double degrees) {
         double running_time = abs(degrees)*rotation_time_conv_fact;
 
         //Get current time in seconds
@@ -115,5 +145,8 @@ public class AutoMover {
         front_right_motor.setPower(0);
         back_left_motor.setPower(0);
         back_right_motor.setPower(0);
+
+        //Used for telemetry and calibration purposes
+        return running_time;
     }
 }
